8a6efb07824c6d72c3a6cfafa9115d524199c6c3
David Jensen <david@jensen47.com>
2012-11-30 01:46:25 -0800

    Added initial user controller and views.

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,13 @@
-module.exports = function(app) {
+var User = require('../models/user');
 
+module.exports = function(app) {
+  // app.all('/user/*', requireAuthentication, loadUser);
+
+  app.get('/user/list', function(req, res){
+    User.find({}).limit(10).exec(function(err, users){
+      if (err)
+        res.send(500, { error: 'Database Error' });
+      res.render('user/list', { userList: users });
+    });
+  });
 }
